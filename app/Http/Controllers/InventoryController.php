7f9b348e29bdfa7b94d7dcd4ad9d92bab554eea9
Wade Tam <tangweimeng@live.com>
2018-10-13 23:27:45 +0800

    First Commit

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Inventory;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $inventories = Inventory::all();
+        $fields = array_slice(array_keys((array)json_decode(json_encode($inventories->first()))),1);
+        return view('inventories.index',compact(['inventories','fields']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Inventory  $inventory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Inventory $inventory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Inventory  $inventory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Inventory $inventory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Inventory  $inventory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Inventory $inventory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Inventory  $inventory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Inventory $inventory)
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+        @for ($i = 1; $i < 5; $i++)
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <span data-feather="bar-chart-2"></span>Figure {{$i}}
+                </a>
+            </li>
+        @endfor
+        </ul>
+    </div>
+</nav>
